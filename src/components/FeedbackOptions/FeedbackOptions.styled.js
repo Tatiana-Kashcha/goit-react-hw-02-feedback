c46d161b717fe4807eb5fcbd859014343b45afdc
Tatiana Kashcha <tkashcha@gmail.com>
2023-06-10 20:27:13 +0300

    Update FeedbackOptions.styled.js

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -14,7 +14,12 @@ export const ListItems = styled.li`
     font-size: 16px;
     font-weight: bold;
     border: 1px solid rgb(219, 216, 216);
-    border-radius: 3px;
+    border-radius: 4px;
     cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.12) 0px 0px 1px, rgba(0, 0, 0, 0.14) 0px 0px 1px,
+      rgba(0, 0, 0, 0.2) 0px 1px 0px;
+    &:active {
+      background-color: rgb(99 149 241);
+    }
   }
 `;
